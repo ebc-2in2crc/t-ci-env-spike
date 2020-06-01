8d90966618884d55f95ac6a55f8c0e283cd5a0a2
ebc-2in2crc <y015h1n80@gmail.com>
2020-06-02 00:00:01 +0900

    add env

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	// コメント
 	fmt.Println("t-ci-env-spike start")
 	fmt.Printf("HOGE=%s\n", os.Getenv("HOGE"))
-	fmt.Printf("FUGA (encrpted)=%s\n", os.Getenv("FUGA"))
+	fmt.Printf("FUGA=%s\n", os.Getenv("FUGA"))
+	fmt.Printf("HOGERA (encrypted)=%s\n", os.Getenv("HOGERA"))
 	fmt.Println("t-ci-env-spike end")
 }
